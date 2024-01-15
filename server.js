8238be695aa29805b967d6b6e49117161b3222f6
tiago-ro <tiago.ro.dev@gmail.com>
2024-01-15 20:06:28 -0300

    added DB

--- server.js
+++ server.js
@@ -1,19 +1,9 @@
-import http from "http";
+import "dotenv/config"
+import app from "./src/app.js";
 
 const PORT = 3000
 
-const rotas = {
-    "/": "Curso de Express API",
-    "/livros": "Entrei na rota livros",
-    "/autores": "entrei na rota autores"
-}
-
-const server = http.createServer((req, res)=>{
-    res.writeHead(200, "content-type: text/plain")
-    res.end(rotas[req.url]);
-});
-
-server.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log("Servidor estutando");
 });
 
--- src/app.js
+++ src/app.js
@@ -1,9 +1,52 @@
 import express from "express";
+import conectaNaDatabase from "./config/dbConnect.js";
+import livro from "./models/Livro.js"
+
+const conexao = await conectaNaDatabase();
+
+conexao.on("error", (erro) => {
+    console.error("erro de conexão", erro);
+});
+
+conexao.once("open", () => {
+    console.log("Conexão com o banco feita com sucesso")
+})
 
 const app = express();
+app.use(express.json());
 
-
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.status(200).send("Curso de Node.js")
 });
 
+app.get("/livros", async (req, res) => {
+    const listaLivros = await livro.find({})
+    res.status(200).json(listaLivros)
+});
+
+app.get("/livros/:id", (req, res) => {
+    const index = req.params.id;
+    const livroEncontrado = livro.findById({id: index});
+    res.status(200).json(livroEncontrado);
+});
+
+
+app.post("/livros", (req, res) => {
+    livros.push(req.body);
+    res.status(201).send("Livro cadastrado com sucesso");
+});
+
+app.put("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.status(200).json(livros);
+});
+
+app.delete("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros.splice(index, 1);
+    res.status(200).send("Livro removido com sucesso");
+});
+export default app;
+
+//mongodb+srv://admin:<password>@cluster0.yapuojr.mongodb.net/
