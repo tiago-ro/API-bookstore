c8a7227fd91425d5b01cdfd68437a014db48c3b8
tiago-ro <tiago.ro.dev@gmail.com>
2024-01-15 23:42:10 -0300

    add livroController and livroRoutes

---
+++ src/controllers/livroController.js
@@ -0,0 +1,56 @@
+import livro from "../models/Livro.js";
+
+class LivroController {
+  
+  static async listarLivros (req, res) {
+    try {
+      const listaLivros = await livro.find({})
+      res.status(200).json(listaLivros)
+    } catch (erro) {
+      res.status(500).json({message: `${erro.message} - Falha na requisição` })  
+    }
+  };
+
+  static async cadastrarLivro (req, res) {
+    try {
+      const novoLivro = await livro.create(req.body);
+      res.status(201).json({ message: "criado com sucesso", livro: novoLivro });
+    } catch (erro) {
+      res.status(500).json({message: `${erro.message} - Falha ao cadastrar livro`});
+    }
+  };
+
+  static async listarLivroPorId (req, res) {
+    try {
+      const id = req.params.id;
+      const livroEncontrado = await livro.findById(id);
+      res.status(200).json(livroEncontrado);
+    } catch (erro) {
+      res.status(500).json({message: `${erro.message} - Falha na requisição do livro `})
+    }
+};
+
+static async atualizaLivro (req, res) {
+    try {
+        const id = req.params.id;
+        await livro.findByIdAndUpdate(id, req.body);
+        res.status(200).json({message: "livro atualizado" });
+    } catch (erro) {
+        res.status(500).json({message: `${erro.message} - Falha na atualização `})
+    }
+  };
+
+  static async excluirLivro (req, res) {
+    try {
+      const id = req.params.id;
+      await livro.findByIdAndDelete(id);
+      res.status(400).json({message: "Livro excluido com sucesso"});
+    } catch (erro) {
+      res.status(500).json({message: `${erro.message} - Falha na exclusão` })  
+    }
+  }
+
+
+};
+
+export default LivroController;
